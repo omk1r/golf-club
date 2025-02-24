81c597ea208146cf5b29c17646ea7ca61c1891cb
Omkar <jadhavpatil2999@gmail.com>
2025-02-24 17:13:06 +0530

    footer responsiveness achieved

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -2,43 +2,43 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="min-h-[40vh] w-full bg-[linear-gradient(to_left_bottom,#119f3a,#ACE022)] relative ">
+    <div className="bg-[linear-gradient(to_left_bottom,#119f3a,#ACE022)] relative w-full min-h-[40vh]">
       <img
         src="https://eiwgew27fhz.exactdn.com/wp-content/themes/puttosaurus/img/dots-footer.svg"
         alt=""
-        className="h-full w-[30%] absolute left-0 object-cover z-0"
+        className="left-0 z-0 absolute w-[30%] h-full object-cover"
       />
-      <div className="h-full relative flex items-center justify-evenly z-[99] py-28">
+      <div className="z-[99] relative flex justify-evenly items-center gap-2 px-4 py-14 lg:py-28 h-full">
         <img
           src="https://eiwgew27fhz.exactdn.com/wp-content/uploads/2023/02/logo-white.svg"
           alt=""
-          className="h-[6vw]"
+          className="h-12 lg:h-[6vw]"
         />
 
-        <div className="flex flex-col ">
-          <span className="text-2xl font-extrabold text-black hover:text-white transition-all duration-300">
+        <div className="flex flex-col gap-2">
+          <span className="font-extrabold text-black hover:text-white text-lg lg:text-2xl transition-all duration-300">
             TOPTRACER RANGE
           </span>
-          <span className="text-2xl font-extrabold text-black hover:text-white transition-all duration-300">
+          <span className="font-extrabold text-black hover:text-white text-lg lg:text-2xl transition-all duration-300">
             {' '}
             CRAZY GOLF
           </span>
-          <span className="text-2xl font-extrabold text-black hover:text-white transition-all duration-300">
+          <span className="font-extrabold text-black hover:text-white text-lg lg:text-2xl transition-all duration-300">
             CAFE`
           </span>
         </div>
-        <div className="flex flex-col ">
-          <span className="text-2xl font-extrabold text-black hover:text-white transition-all duration-300">
+        <div className="flex flex-col gap-2">
+          <span className="font-extrabold text-black hover:text-white text-lg lg:text-2xl transition-all duration-300">
             EVENTS
           </span>
-          <span className="text-2xl font-extrabold text-black hover:text-white transition-all duration-300">
+          <span className="font-extrabold text-black hover:text-white text-lg lg:text-2xl transition-all duration-300">
             GOLF LESSONS
           </span>
-          <span className="text-2xl font-extrabold text-black hover:text-white transition-all duration-300">
+          <span className="font-extrabold text-black hover:text-white text-lg lg:text-2xl transition-all duration-300">
             CONTACT US
           </span>
         </div>
-        <div className="w-[20%] text-black font-semibold">
+        <div className="w-[20%] font-semibold text-black text-xs sm:text-base">
           A20, Sidcup Bypass Chislehurst Kent BR7 6RP Tel: 0208 309 0181 Get
           Directions
         </div>
